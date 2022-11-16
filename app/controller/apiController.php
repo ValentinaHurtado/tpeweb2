0f8f5c43254532559bcb2239db5e1af0fd609795
Valentina Hurtado <vhurtado@alumnos.exa.unicen.edu.ar>
2022-11-15 23:46:25 -0300

    comentarios

--- app/controller/apiController.php
+++ app/controller/apiController.php
@@ -7,7 +7,6 @@ class ApiController{
     private $model;
     private $generosModel;
     private $view;
-    private $authHelper;
 
     private $data;
 
@@ -22,8 +21,13 @@ class ApiController{
         return json_decode($this->data);
     }
 
+    /*verifica que los parametros pasados mediante la url sean correctos y permitan ordenar, filtrar, paginar
+        guarda en un arreglo los nombres de las columnas 
+            para no tener que chequear uno por uno y poder usar funcion in_array
+            verifica que los valores del orderBy y filterBy este entre este
+        verifica que exista la forma de ordenamiento qque se paso y que la pagina y el limita sean un nro>0*/
     public function verifyGet($orderBy, $orderMode, $filterBy, $filterValue, $page, $limit){
-        $columns=[
+        $columns=[ 
             "id_libros",
             "titulo",
             "autores",
@@ -53,6 +57,9 @@ class ApiController{
         }
     }
 
+    /*obtiene una lista de los libros, pide parametros para ordenar, filtrar y paginar la lista
+        si tiene filtrado se llama a la funcion que los lista por filtro, sino se llama a la sin filtrar
+        se devuelve la lista de libros*/
     public function getAll($params=null){
         $orderBy=$_GET["orderBy"] ?? "id_libros";
         $orderMode=$_GET["orderMode"] ?? "asc";
@@ -64,6 +71,7 @@ class ApiController{
         $this->verifyGet($orderBy, $orderMode, $filterBy, $filterValue, $page, $limit);
         
         $row = $page*$limit - $limit;
+        $filterValue=str_replace('-', ' ', $filterValue);
         if($filterBy==null){
             $libros=$this->model->getAll($orderBy, $orderMode, $limit, $row);
         }
@@ -73,6 +81,9 @@ class ApiController{
         return $this->view->response($libros, 200);
     }
 
+    /*obtiene un libro determinado por su id
+        obtiene el id por la url
+        llama a la funcion del model que lo busca, si existe lo devuelve sino devuelve un error*/
     public function getLibroById($params=null){
         $id=$params[':ID'];
         $libro=$this->model->getLibroInd($id);
@@ -84,11 +95,16 @@ class ApiController{
         }
     }
 
+    /*verifica si el id de genero que se puso al agregar un libro o modificarlo existe*/
     public function verificarIdGen($idGen){
-        $generos=$this->generosModel->getAll();
+        $generos=$this->generosModel->getAllIds();
         return in_array($idGen, $generos);
     }
 
+    /*agrega libros a la db 
+        verifica que tenga todas las columnas llenas y envia error si no lo estan
+        verifica que el valor ingrsado en fk_generos corresponda a un id de generos sino muestra error
+        en caso de que este todo bien muestra msj de que se inserto el libro*/
     public function addLibro($params=null){
         $libro=$this->getData();
         if(empty($libro->Titulo)||empty($libro->Autores)||empty($libro->Anio)||empty($libro->Precio)||empty($libro->Generos_fk)){
@@ -106,6 +122,10 @@ class ApiController{
         }
     }
 
+    /*edita un libro de la db usando su id que recibe como parametro en la url
+        llama al libro con este id mediante la funcion del model
+        si existe, estan todos los datos completos y la fk de generos esta bien, se modifica
+        sino muestra un error*/ 
     public function editLibro($params=null){
         $id = $params[':ID'];
         $data=$this->getData();
@@ -130,6 +150,8 @@ class ApiController{
         
     }
     
+    /*elimina un libro de la db mediante su id que se pasa como parametro en la url
+        cheuqeamos que el libro exista si existe se elimina sino muestra error */
     public function deleteLibro($params=null){
         $id=$params[':ID'];
         $libro=$this->model->getLibroInd($id);
--- app/model/generosModel.php
+++ app/model/generosModel.php
@@ -10,7 +10,7 @@ class GenerosModel{
     /*llama a todos los generos de mi db, y agarra solo los id de la tabla
         se prepara sentencia en la que llamo a los id y se ejecuta
         se guarda en una variable el resultado de la busqueda(se pide solo la columna) y se devuelve*/
-    public function getAll(){
+    public function getAllIds(){
         $query = $this->db->prepare("SELECT Id_generos FROM generos");
         $query -> execute();
         $generos = $query->fetchAll(PDO::FETCH_COLUMN);
